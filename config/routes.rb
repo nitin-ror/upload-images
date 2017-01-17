8a0b279a9157dffc0e05c43776bc5df87196c22a
Nitin Rakesh <nitinrakesh.332@gmail.com>
2017-01-17 16:12:39 +0530

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :articles, :upload_images#, :products
+  root 'products#new'
   resources :products do
   	collection do
   		post 'temp_image_update' => 'products#temp_image_update', as: 'temp_image_update'
